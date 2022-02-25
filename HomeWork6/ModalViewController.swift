74ec7f869d5b36747de3a40ac67d8378c1e0f549
budanovDmitriy <lllytnick1@gmail.com>
2022-02-25 09:14:55 +0300

    Initial commit

---
+++ HomeWork6/ModalViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ModalViewController.swift
+//  HomeWork6
+//
+//  Created by Dmitriy Budanov on 23.02.2022.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    @IBOutlet weak var runningRoundView: UIView!
+
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupSubviews()
+        
+    }
+        // Do any additional setup after loading the view.
+
+    private func setupSubviews() {
+        runningRoundView.backgroundColor = .green
+        runningRoundView.layer.cornerRadius = 25.0
+        runningRoundView.frame =
+        CGRect( x:super.view.bounds.width / 2 - 25, y: super.view.bounds.height / 2 - 25 , width: 50, height: 50)
+        super.view.addSubview(runningRoundView)
+        
+        arrowUp.addTarget(self, action: #selector(arrowDowning), for: .touchUpInside)
+        arrowDown.addTarget(self, action: #selector(arrowDowning), for: .touchUpInside)
+        arrowLeft.addTarget(self, action: #selector(arrowLefting), for: .touchUpInside)
+        arrowRight.addTarget(self, action: #selector(arrowRighting), for: .touchUpInside)
+    }
+    @IBOutlet weak var arrowUp: UIButton!
+    
+    @IBOutlet weak var arrowDown: UIButton!
+    @IBOutlet weak var arrowLeft: UIButton!
+    @IBOutlet weak var arrowRight: UIButton!
+    
+    
+    
+        @objc private func arrowUping(_ sender: Any) {
+            if
+                runningRoundView.frame.minY > 25 {
+            runningRoundView.frame = CGRect(x:runningRoundView.frame.origin.x  , y: runningRoundView.frame.origin.y + 10, width: 50, height: 50)
+            }
+        
+    }
+        @objc private func arrowDowning(_ sender: Any) {
+            if runningRoundView.frame.maxY < self.view.bounds.height - 25 {
+                runningRoundView.frame = CGRect(x:runningRoundView.frame.origin.x  , y: runningRoundView.frame.origin.y + 10, width: 50, height: 50)
+                
+            }
+    }
+   
+        @objc private func arrowRighting(_ sender: Any) {
+            if runningRoundView.frame.maxX < self.view.bounds.width - 25  {
+            runningRoundView.frame = CGRect(x:runningRoundView.frame.origin.x + 20 , y: runningRoundView.frame.origin.y, width: 50, height: 50)
+            }
+    }
+        @objc private func arrowLefting(
+        _ sender: Any) {
+            if runningRoundView.frame.maxX >  25  {
+            runningRoundView.frame = CGRect(x:runningRoundView.frame.origin.x - 10 , y: runningRoundView.frame.origin.y, width: 50, height: 50)
+            }
+    }
+}
---
+++ HomeWork6/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  HomeWork6
+//
+//  Created by Dmitriy Budanov on 23.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    let catToy = UIButton()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSubviews()
+
+    }
+    
+    private func setupSubviews() {
+        catToy.backgroundColor = .red
+        self.view.addSubview(catToy)
+        let superVievHeight = self.view.bounds.height
+        let superVievWidth = self.view.bounds.width
+        catToy.frame = CGRect(x: Double(superVievWidth / 2 - 25), y: Double(superVievHeight / 2 - 25), width: 50, height: 50)
+        
+        catToy.addTarget(self, action: #selector(btnTouched), for: .touchUpInside)
+        self.view.addSubview(catToy)
+    }
+    
+    @objc private func btnTouched(){
+        let superVievHeight = self.view.bounds.height
+        let superVievWidth = self.view.bounds.width
+        let maxY = superVievHeight - 50
+        let maxX = superVievWidth - 50
+        catToy.frame = CGRect(
+            x: Double(Int.random(in: 0..<Int(maxX))),
+            y:Double(Int.random(in:0..<Int(maxY))),
+        width: 50,
+        height: 50)
+        
+        
+    }
+    
+    
+
+}
+
